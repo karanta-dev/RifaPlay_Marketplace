ab54ce7d1664ae7bbb7cbe898163f5f32450bf4a
Samuel Cedeno <samuelcd311@outlook.es>
2025-09-23 16:08:36 -0400

    correction_nav_bar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="min-h-screen bg-gray-50">
-    <Navbar />
-
-  <RouterView />
+    <div class="-mt-8">
+      <Navbar />
+    </div>
+    <RouterView />
   </div>
-
 </template>
 
 <script setup lang="ts">
 import Navbar from './components/Navbar.vue'
-
 </script>
